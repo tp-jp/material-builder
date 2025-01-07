201db32f6bca05ad76e5731c21724ba127149ea7
tp.jp <chronokreuz@gmail.com>
2025-01-07 19:46:57 +0900

    Shader設定の統合

--- Packages/com.tp-lab.material-builder/Editor/ShaderSettings.cs
+++ Packages/com.tp-lab.material-builder/Editor/ShaderSettings.cs
@@ -8,16 +8,12 @@ namespace TpLab.MaterialBuilder.Editor
     [CreateAssetMenu(menuName = "TpLab/MaterialBuilder/CreateShaderSettings", fileName = "ShaderSettings")]
     public class ShaderSettings : ScriptableObject
     {
-        static readonly string ShaderSettingPath = "Packages/com.tp-lab.material-builder/Runtime/ShaderSettings.asset";
-
-        static readonly string ExtraShaderSettingPath =
-            "Packages/com.tp-lab.material-builder/Runtime/ExtraShaderSettings.asset";
+        static readonly string SettingFilePath = "Packages/com.tp-lab.material-builder/Runtime/ShaderSettings.asset";
 
         [SerializeField]
         public ShaderSetting[] shaderSettings;
 
         static ShaderSettings _instance;
-        static ShaderSettings _extraShaderSettings;
 
         /// <summary>
         /// インスタンス
@@ -28,23 +24,10 @@ namespace TpLab.MaterialBuilder.Editor
             {
                 if (_instance == null)
                 {
-                    _instance = AssetDatabase.LoadAssetAtPath<ShaderSettings>(ShaderSettingPath);
+                    _instance = AssetDatabase.LoadAssetAtPath<ShaderSettings>(SettingFilePath);
                 }
 
                 return _instance;
-            }
-        }
-
-        static ShaderSettings ExtraShaderSettings
-        {
-            get
-            {
-                if (_extraShaderSettings == null)
-                {
-                    _extraShaderSettings = AssetDatabase.LoadAssetAtPath<ShaderSettings>(ExtraShaderSettingPath);
-                }
-
-                return _extraShaderSettings;
             }
         }
 
@@ -52,8 +35,7 @@ namespace TpLab.MaterialBuilder.Editor
         /// シェーダー設定一覧
         /// </summary>
         public static ShaderSetting[] AllShaderSettings =>
-            Instance.shaderSettings.Concat(ExtraShaderSettings.shaderSettings.Where(x => Shader.Find(x.shaderName)))
-                .ToArray();
+            Instance.shaderSettings.Where(x => Shader.Find(x.shaderName)).ToArray();
     }
 
     [Serializable]
